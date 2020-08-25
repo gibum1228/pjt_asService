683f9817011de2544e1ea11c09b0b647f5835576
김기범 <38284922+gibum1228@users.noreply.github.com>
2020-08-25 23:45:05 +0900

    mySQL

---
+++ html/js/db_mysql.js
@@ -0,0 +1,19 @@
+// DB 초기화
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '111112',
+  database : 'as_service'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
+// ================================================================================
